368cc5ae3a8f7f0c2e77bb3209666656bb053d6c
CreArthurYellowTeapot <arthur@yellow-teapot.com>
2022-04-28 14:53:18 +0200

    DialogueAndTargetEvent

--- Scripts/Scriptable Objects/Core/Variables/Definitions/VariableListenerBase.cs
+++ Scripts/Scriptable Objects/Core/Variables/Definitions/VariableListenerBase.cs
@@ -1,6 +1,8 @@
+using System.Text;
 using Sirenix.OdinInspector;
 using Toolbox.ScriptableObjects.Events;
 using UltEvents;
+using UnityEditor;
 using UnityEngine;
 
 namespace Toolbox.ScriptableObjects.Variables
@@ -8,9 +10,29 @@ namespace Toolbox.ScriptableObjects.Variables
     [ExecuteAlways]
     public abstract class VariableListenerBase<T, TVariable> : MonoBehaviour where TVariable : VariableSOBase<T>
     {
-        [SerializeField, AssetSelector, Required("A variable must be assigned for this listener!")] 
+        [SerializeField, AssetSelector, Required("A variable must be assigned for this listener!")]
         protected TVariable variable;
+        [SerializeField, ShowIf("@variable==null"), InlineButton("Create")]
+        string soName;
+        [Searchable]
         public UltEvent<T> events;
+
+        protected virtual void Create(string soName)
+        {
+            CreateSOAsset(soName);
+        }
+
+        protected void CreateSOAsset(string path)
+        {
+            TVariable newSO = ScriptableObject.CreateInstance<TVariable>();
+            // path has to start at "Assets"
+            path = "Assets/" + path;
+            AssetDatabase.CreateAsset(newSO, path);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = newSO;
+        }
     
         void Subscribe()
         {
--- Scripts/Scriptable Objects/Core/Variables/Listeners/StringVariableListener.cs
+++ Scripts/Scriptable Objects/Core/Variables/Listeners/StringVariableListener.cs
@@ -1,4 +1,9 @@
+using UnityEngine;
+
 namespace Toolbox.ScriptableObjects.Variables
 {
-    public class StringVariableListener : VariableListenerBase<string, StringVariable> {}
+    public class StringVariableListener : VariableListenerBase<string, StringVariable>
+    {
+        protected override void Create(string soName) => CreateSOAsset("StringV_");
+    }
 }
