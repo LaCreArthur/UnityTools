a1eee92318f81ce415fadc447c2e7f8a97665280
LaCreArthur <le-tutur@hotmail.fr>
2023-02-10 09:20:06 +0100

    removed dreamteck spline (used unity new one), removed polysky, merged proto and main scene

--- Scripts/Scriptable Objects/Extensions/TransformVariableSetter.cs
+++ Scripts/Scriptable Objects/Extensions/TransformVariableSetter.cs
@@ -2,6 +2,11 @@
 
 namespace AS.Toolbox.ScriptableObjects
 {
+    /// <summary>
+    ///     Simple component that sets a TransformVar to the transform of the GameObject.
+    ///     This is useful for referencing the transform of a GameObject without hard referencing it in the inspector.
+    ///     This is also the only way to reference a scene transform in a prefab.
+    /// </summary>
     public class TransformVariableSetter : MonoBehaviour
     {
         public TransformVar transformVar;
