0a723632bc468cbdfc908acb21c2975805ccfdc5
LaCreArthur <le-tutur@hotmail.fr>
2023-02-12 20:26:03 +0100

    add summary

--- Runtime/PrefabPool/PopulatePoolWithChild.cs
+++ Runtime/PrefabPool/PopulatePoolWithChild.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 namespace AS.Toolbox.PrefabPool
 {
+    /// <summary>
+    ///     A simple component to populate a pool with the child objects of a game object
+    ///     The child objects must be instances of the prefab to be pooled
+    ///     Useful for populating a pool before gameplay starts to avoid instantiation spikes
+    /// </summary>
     public class PopulatePoolWithChild : MonoBehaviour
     {
         [SerializeField]
