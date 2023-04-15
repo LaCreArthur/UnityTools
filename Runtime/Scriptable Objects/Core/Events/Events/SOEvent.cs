1fd0ed77b7901e9cb5964ed83f9ac316dd834798
LaCreArthur <le-tutur@hotmail.fr>
2023-04-15 13:21:33 +0200

    no longer need to reference the listener target in Add/Remove

--- Runtime/Scriptable Objects/Core/Events/Events/SOEvent.cs
+++ Runtime/Scriptable Objects/Core/Events/Events/SOEvent.cs
@@ -10,20 +10,18 @@ namespace AS.Toolbox.ScriptableObjects
     [CreateAssetMenu(menuName = "Scriptable Objects/Events/Event", fileName = "E_")]
     public class SOEvent : SOEventBase, ISOEvent<UnityEvent>
     {
-        [TitleGroup("Listener"), HideLabel, InlineProperty, HideReferenceObjectPicker, OnInspectorGUI("RemoveNullElements")]
+        [TitleGroup("Listener"), HideLabel, InlineProperty, HideReferenceObjectPicker, OnInspectorGUI("RemoveNullElements")]    
         public ReferencedCallbacks listeners = new ReferencedCallbacks();
 
         void RemoveNullElements() => listeners.RemoveAll(l => l.reference == null);
 
         public void Add(ReferencedEvent<UnityEvent> rEvent) => listeners.Add(rEvent);
-        public void Add(UnityEvent uEvent, Object listener) => listeners.Add(new ReferencedEvent<UnityEvent>(uEvent, listener));
-        public void Add(Action callback, Object listener) => listeners.Add(callback, listener);
+        public void Add(Action callback) => listeners.Add(callback, (Object)callback.Target);
 
         public void Remove(ReferencedEvent<UnityEvent> rEvent) => listeners.Remove(rEvent);
-        public void Remove(UnityEvent uEvent, Object listener) => listeners.Remove(new ReferencedEvent<UnityEvent>(uEvent, listener));
-        public void Remove(Action callback, Object listener) => listeners.Remove(callback, listener);
+        public void Remove(Action callback) => listeners.Remove(callback, (Object)callback.Target);
 
-        [TitleGroup("Debug"), Button]
+        [TitleGroup("Debug"), Button] 
         public void Raise()
         {
             if (logRaise)
