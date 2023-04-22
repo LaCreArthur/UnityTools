fda3a0201513f0a7f956f2ec95a8141a11ada3f8
LaCreArthur <le-tutur@hotmail.fr>
2023-04-22 14:43:51 +0200

    added BoolVarListener.cs

--- Runtime/Scriptable Objects/Core/Variables/Listeners/BoolVarListener.cs
+++ Runtime/Scriptable Objects/Core/Variables/Listeners/BoolVarListener.cs
@@ -1,4 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
 namespace AS.Toolbox.ScriptableObjects
 {
-    public class BoolVarListener : VarListenerBase<bool, BoolVar> {}
+    public class BoolVarListener : VarListenerBase<bool, BoolVar>
+    {
+        [SerializeField] UnityEvent onTrue, onFalse;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            variable.onChange.Add(OnVariableChanged, this);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            variable.onChange.Remove(OnVariableChanged, this);
+        }
+
+        void OnVariableChanged(bool value)
+        {
+            if (value)
+                onTrue?.Invoke();
+            else
+                onFalse?.Invoke();
+        }
+    }
 }
