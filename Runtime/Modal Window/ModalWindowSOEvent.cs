125368fc17c08823ca8f2b9af983acefe31d3670
LaCreArthur <le-tutur@hotmail.fr>
2023-08-11 17:15:25 +0200

    improvement: removing unused scripts

--- Runtime/Modal Window/ModalWindowSOEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using AS.Toolbox.ScriptableObjects;
-using UnityEngine;
-
-namespace AS.Toolbox.ModalWindow
-{
-    [CreateAssetMenu(menuName = "Scriptable Objects/ModalWindowEvent", fileName = "ModalWindowEvent", order = 0)]
-    public class ModalWindowSOEvent : LoadedSOEvent<ModalWindowTemplate> {}
-}
