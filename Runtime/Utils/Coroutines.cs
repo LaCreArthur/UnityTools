096207a6016950cfe6816a55a9a111abceb6b9f8
LaCreArthur <le-tutur@hotmail.fr>
2023-08-15 17:09:17 +0200

    new Coroutines static class for shared WaitAFrameAnd

---
+++ Runtime/Utils/Coroutines.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace AS.Toolbox.Utils
+{
+    public static class Coroutines
+    {
+        public static IEnumerator WaitAFrameAnd(Action callback)
+        {
+            yield return new WaitForEndOfFrame();
+            callback?.Invoke();
+        }
+    }
+}
