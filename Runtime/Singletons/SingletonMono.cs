19afb4cd86e6a14d98268f9a19a5bc44d32be5a3
LaCreArthur <le-tutur@hotmail.fr>
2024-08-22 13:33:36 +0200

    chore: removed isAlive from SingletonMono as it was not reliable and useful

--- Runtime/Singletons/SingletonMono.cs
+++ Runtime/Singletons/SingletonMono.cs
@@ -3,28 +3,14 @@ using UnityEngine;
 
 namespace AS.Toolbox.Singletons
 {
-    /// <summary>
-    ///     SingletonMB's design intentions:
-    ///     - Singleton is already instantiated in the scene at game start (preferably, but it can be instantiated on-demand).
-    ///     - The singleton instance is not destroyable between scene changes.
-    ///     - The instantiated singleton should be active ("FindObjectofType" will not find it if disabled).
-    ///     - "OnAwake" is called once, either at the singleton MonoBehaviour's "Awake" message call or at the first get of the
-    ///     instance.
-    ///     - Don't call from a non-main thread.
-    ///     - "Awake" should not be defined in derived classes.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
     public abstract class SingletonMono<T> : MonoBehaviour where T : SingletonMono<T>
     {
         static T s_instance;
-        bool _alive;
-
         bool _isAwoken;
 
         public static T Instance
         {
-            get
-            {
+            get {
 #if UNITY_EDITOR
                 if (!Application.isPlaying)
                 {
@@ -49,8 +35,6 @@ namespace AS.Toolbox.Singletons
             }
         }
 
-        public static bool IsAlive => s_instance != null && s_instance._alive;
-
         void Awake()
         {
             if (s_instance != null && s_instance != this)
@@ -62,13 +46,9 @@ namespace AS.Toolbox.Singletons
             else
             {
                 s_instance = this as T;
-                _alive = true;
                 Init();
             }
         }
-
-        void OnDestroy() => _alive = false;
-        void OnApplicationQuit() => _alive = false;
 
         void Init()
         {
