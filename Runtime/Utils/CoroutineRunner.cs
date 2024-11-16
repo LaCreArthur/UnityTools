d00df1d9a5ecb8ba7dc0639ac94d55dd54937ea7
LaCreArthur <le-tutur@hotmail.fr>
2024-11-16 15:04:50 +0100

    feat: CoroutineRunner singleton to call coroutines from anywhere

---
+++ Runtime/Utils/CoroutineRunner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace AS.Toolbox.Utils
+{
+    public class CoroutineRunner : MonoBehaviour
+    {
+        static CoroutineRunner Instance { get; set; }
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public static void WaitForEndOfFrame(Action callback) => Instance.StartCoroutine(Coroutines.WaitForEndOfFrame(callback));
+        public static void WaitForSecond(float time, Action callback) => Instance.StartCoroutine(Coroutines.WaitForSecond(time, callback));
+    }
+}
--- Runtime/Utils/Coroutines.cs
+++ Runtime/Utils/Coroutines.cs
@@ -18,4 +18,5 @@ namespace AS.Toolbox.Utils
             callback?.Invoke();
         }
     }
+
 }
