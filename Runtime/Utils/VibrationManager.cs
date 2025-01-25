a33c0281a12eaf150a372dfd8a71aaf857192c55
LaCreArthur <le-tutur@hotmail.fr>
2025-01-25 15:31:31 +0100

    chore: made vibration manager a singleton so it is persistent over scenes

--- Runtime/Utils/VibrationManager.cs
+++ Runtime/Utils/VibrationManager.cs
@@ -1,12 +1,13 @@
 ﻿using AS.Toolbox;
 using AS.Toolbox.ScriptableObjects;
+using AS.Toolbox.Singletons;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class VibrationManager : MonoBehaviour
+public class VibrationManager : SingletonMono<VibrationManager>
 {
-    [SerializeField, Required] BoolVar isHapticVar;
-    void Awake() => OnHapticChange();
+    [SerializeField] [Required] BoolVar isHapticVar;
     void OnEnable() => isHapticVar.AddOnChange(OnHapticChange);
+    void Start() => OnHapticChange();
     void OnHapticChange() => Vibrations.IsHaptic = isHapticVar.v;
 }
