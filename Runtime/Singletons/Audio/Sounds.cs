7d6cebe42f3a0e3cd24bc2d0f16989cea555d8e9
LaCreArthur <le-tutur@hotmail.fr>
2025-04-06 13:32:52 +0200

    feat: added crash, repair and building upgrade sfx, added sound SOs to item SOs

--- Runtime/Singletons/Audio/Sounds.cs
+++ Runtime/Singletons/Audio/Sounds.cs
@@ -9,15 +9,20 @@ namespace AS.Toolbox.Singletons.Audio
         [SerializeField] SoundSO flap;
         [SerializeField] SoundSO takeoff;
         [SerializeField] SoundSO unlock;
-        [SerializeField] SoundSO wow;
         [SerializeField] SoundSO swipe;
+        [SerializeField] SoundSO crash;
+        [SerializeField] SoundSO repair;
+        [SerializeField] SoundSO buildingUpgrade;
 
         public static SoundSO AudioEnabled => Instance.audioEnabled;
         public static SoundSO Click => Instance.click;
         public static SoundSO Flap => Instance.flap;
         public static SoundSO Takeoff => Instance.takeoff;
         public static SoundSO Unlock => Instance.unlock;
-        public static SoundSO Wow => Instance.wow;
+
         public static SoundSO Swipe => Instance.swipe;
+        public static SoundSO Crash => Instance.crash;
+        public static SoundSO Repair => Instance.repair;
+        public static SoundSO BuildingUpgrade => Instance.buildingUpgrade;
     }
 }
