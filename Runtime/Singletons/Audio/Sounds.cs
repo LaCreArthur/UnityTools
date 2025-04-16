0a2719450ddc5cd23108c091f22aa1e267eabd3b
LaCreArthur <le-tutur@hotmail.fr>
2025-04-16 02:27:29 +0200

    feat: added and improved sfx

--- Runtime/Singletons/Audio/Sounds.cs
+++ Runtime/Singletons/Audio/Sounds.cs
@@ -14,19 +14,20 @@ namespace AS.Toolbox.Singletons.Audio
         [SerializeField] SoundSO repair;
         [SerializeField] SoundSO swipe;
         [SerializeField] SoundSO takeoff;
+        [SerializeField] SoundSO bark;
         [SerializeField] SoundSO unlock;
 
         public static SoundSO AudioEnabled => Instance.audioEnabled;
-        public static SoundSO Click => Instance.click;
-        public static SoundSO Flap => Instance.flap;
-        public static SoundSO Takeoff => Instance.takeoff;
-        public static SoundSO Unlock => Instance.unlock;
-
-        public static SoundSO Swipe => Instance.swipe;
-        public static SoundSO Crash => Instance.crash;
-        public static SoundSO Repair => Instance.repair;
         public static SoundSO BuildingUpgrade => Instance.buildingUpgrade;
         public static SoundSO ZoneUnlocked => Instance.zoneUnlocked;
+        public static SoundSO Click => Instance.click;
+        public static SoundSO Crash => Instance.crash;
+        public static SoundSO Flap => Instance.flap;
         public static SoundSO HelmetLost => Instance.helmetLost;
+        public static SoundSO Repair => Instance.repair;
+        public static SoundSO Swipe => Instance.swipe;
+        public static SoundSO Takeoff => Instance.takeoff;
+        public static SoundSO Bark => Instance.bark;
+        public static SoundSO Unlock => Instance.unlock;
     }
 }
